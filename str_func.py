9548fa044163a5b7b2803bf72988ccca27e39bae
IliaReykov <arhipiv1@gmail.com>
2024-02-09 20:44:25 +0400

    str1

--- str_func.py
+++ str_func.py
@@ -1,9 +1,5 @@
 stroka = input()
 print(stroka.upper())
 """
-<<<<<<<<< Temporary merge branch 1
   Функция принимает слово и делает все буквы заглавными
-=========
-Функция принимает слово и делает все буквы заглавными
->>>>>>>>> Temporary merge branch 2
 """
